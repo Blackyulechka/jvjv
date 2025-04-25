6e9a64329f405d69e29bfc2212409776c543d4ec
юля <123@mail.ru>
2025-04-25 22:51:01 +0300

    не доделан

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class User{
+    String Name;
+    String Surname;
+    String email;
+    int id;
+    public User(String Name,String Surname,String email,int id){
+        this.Name=Name; this.Surname=Surname; this.email=email; this.id=id;
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        //z
+    }
+    }
