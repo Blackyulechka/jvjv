7b50874784630f5ba19baa518f99ff0bad653695
юля <123@mail.ru>
2025-06-22 18:28:29 +0300

    система вб

--- Wildberries/WildberriesSystemMain.java
+++ Wildberries/WildberriesSystemMain.java
@@ -4,8 +4,8 @@ public class WildberriesSystemMain {
     public static void main(String[] args) {
         User user = new User(12345, 10);
         Basket<Product> basket = new Basket<>();
-        Product jeans = new Jeans("Джинсы", 12345, 5000, 5);
-        Product cream = new Cream("Крем", 54321, 1000, 0);
+        Product jeans = new Jeans("Джинсы", 12345, 5000);
+        Product cream = new Cream("Крем", 54321, 1000);
 
         basket.addProduct(jeans, 2);
         basket.addProduct(cream, 1);
